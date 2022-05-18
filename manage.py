5b3bdf8101d8f3f2c5b854c9ee669eb7feac235c
Mingfei <shenmingfeid@gmail.com>
2022-05-18 14:02:01 +0800

    Mingfei: Add scrapyd cmd.

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ import sys
 
 
 def main():
+    os.system('scrapyd')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cs65_4_1.settings')
     try:
         from django.core.management import execute_from_command_line
