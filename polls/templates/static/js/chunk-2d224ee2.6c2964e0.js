784a099db5ae228a233fc5af2aafd8fe48d27446
Mingfei <shenmingfeid@gmail.com>
2022-06-03 09:37:36 +0800

    Remove a health direct crawler

--- polls/templates/static/js/chunk-2d224ee2.6c2964e0.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d224ee2"],{e1dd:function(n,e,u){"use strict";u.r(e);var l,c,d=u("2877"),o={},p=Object(d["a"])(o,l,c,!1,null,null,null);e["default"]=p.exports}}]);
-//# sourceMappingURL=chunk-2d224ee2.6c2964e0.js.map
