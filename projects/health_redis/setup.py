9be2f8aabe88ba92c99f8f42a29a68edc77387da
Mingfei <shenmingfeid@gmail.com>
2022-06-07 18:32:00 +0800

    modify scrapy project

--- projects/health_redis/setup.py
+++
@@ -1,8 +0,0 @@
-# Automatically created by: gerapy
-from setuptools import setup, find_packages
-setup(
-    name='health_redis',
-    version='1.0',
-    packages=find_packages(),
-    entry_points={'scrapy':['settings=health_redis.settings']},
-)
